6a1212d323a61504db4a0d71f7a30579899ddc78
Ramakant Gangwar <gangwar.ramakant@gmail.com>
2023-01-17 15:30:47 +0530

    Use VoyagerCore as parent

--- src/Http/Controllers/VoyagerDataTypeSettingsController.php
+++ src/Http/Controllers/VoyagerDataTypeSettingsController.php
@@ -3,7 +3,7 @@
 namespace Joy\VoyagerDataTypeSettings\Http\Controllers;
 
 use Joy\VoyagerDataTypeSettings\Http\Traits\CrudActions;
-use TCG\Voyager\Http\Controllers\Controller as TCGVoyagerController;
+use Joy\VoyagerCore\Http\Controllers\Controller as TCGVoyagerController;
 
 class VoyagerDataTypeSettingsController extends TCGVoyagerController
 {
